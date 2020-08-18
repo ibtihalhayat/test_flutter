87b0561b1ef78947db62593941b0994a9d879127
ibtihal <ibtihalhayat0@gmail.com>
2020-08-18 11:56:03 +0100

    App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,16 @@ class _Home extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Colors.indigo,
+      backgroundColor: Colors.white60,
+      appBar: new AppBar(
+        title: new Text('Mon Application'),
+        leading: new Icon(Icons.arrow_back_ios), // ce qui sera avant le titre à gauche
+        actions: [
+          new Icon(Icons.account_circle) // ce qui sera après le titre
+        ],
+        centerTitle: true,
+        elevation: 50.0, //l'ambre au dessous de la App Bar
+      ),
     );
   }
 }
