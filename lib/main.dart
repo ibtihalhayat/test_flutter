40ff8f16445201c354451afaed06b6837e177d69
ibtihal <ibtihalhayat0@gmail.com>
2020-08-18 11:32:53 +0100

    MaterialApp et Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,35 @@
 import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'first app',
+      theme: new ThemeData(
+        primarySwatch: Colors.amber
+      ),
+      debugShowCheckedModeBanner: false,
+      home: new Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState(){
+    return new _Home() ;
+  }
+}
+
+class _Home extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.indigo,
+    );
+  }
+}
